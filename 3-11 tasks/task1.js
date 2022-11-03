dff828ce0dfa8e48eda9d5b8d0d8b0f539f471ca
MALEKOBEIDAT <115452586+MALEKOBEIDAT@users.noreply.github.com>
2022-11-03 03:41:58 -0700

    j

---
+++ 3-11 tasks/task1.js
@@ -0,0 +1,46 @@
+var mark;
+
+mark = prompt( "Please input the mark(%)" );
+
+if(mark<50){
+    document.write("Fail")
+
+}
+
+else if(mark>=50 && mark<=60){
+    document.write("D")
+
+
+}
+
+else if(mark>=60 && mark<=69){
+    document.write("C")
+
+
+}
+
+else if(mark>=70 && mark<=79){
+    document.write("B")
+
+
+}
+
+else if(mark>=80 && mark<=89){
+    document.write("A")
+
+
+}
+
+else if(mark>=90 && mark<=99){
+    document.write("A+")
+
+
+}
+
+
+
+
+
+
+
+
---
+++ 3-11 tasks/task2.js
@@ -0,0 +1,2 @@
+const a=3.4
+document.write(2*a)
