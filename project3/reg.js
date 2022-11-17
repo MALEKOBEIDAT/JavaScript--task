114518293919ef55528aafb8db306f46b4dafadd
MALEKOBEIDAT <115452586+MALEKOBEIDAT@users.noreply.github.com>
2022-11-17 03:28:34 -0800

    index file

---
+++ project3/reg.js
@@ -0,0 +1,26 @@
+function signup(e){
+event.preventDefault();
+
+var email=document.getElementById("email").value;
+var username=document.getElementById("username").value;
+var password=document.getElementById("password").value;
+
+
+
+
+var user ={
+         email:email,
+         username:username,
+        
+         password:password,
+
+
+     };
+
+     var json=JSON.stringify(user);
+     localStorage.setItem(username,json);
+     console.log('user added');
+
+
+
+}
